6c5af8c5924c8cbda9ed7278421a97829eb37f62
InvalidJoker <0limitdev@gmail.com>
2025-12-01 14:20:52 +0100

    add day 1 part 2

--- src/main/kotlin/tasks/2025/1.kt
+++ src/main/kotlin/tasks/2025/1.kt
@@ -2,14 +2,6 @@ package de.joker.tasks.`2025`
 
 import de.joker.utils.day
 
-/*
-The actual password is the number of times the dial is left pointing at 0 after any rotation in the sequence.
-
-The dial starts by pointing at 50.
-
-Because the dial is a circle, turning the dial left from 0 one click makes it point at 99. Similarly, turning the dial right from 99 one click makes it point at 0.
- */
-
 fun main() = day<List<Pair<Char, Int>>, Int>(2025, 1) {
     input {
         it.map {
@@ -20,6 +12,14 @@ fun main() = day<List<Pair<Char, Int>>, Int>(2025, 1) {
         }
     }
 
+
+    /*
+    The actual password is the number of times the dial is left pointing at 0 after any rotation in the sequence.
+
+    The dial starts by pointing at 50.
+
+    Because the dial is a circle, turning the dial left from 0 one click makes it point at 99. Similarly, turning the dial right from 99 one click makes it point at 0.
+     */
     part(1, 3) {
         var position = 50
         var zeroCount = 0
@@ -33,6 +33,29 @@ fun main() = day<List<Pair<Char, Int>>, Int>(2025, 1) {
 
             if (position == 0) {
                 zeroCount++
+            }
+        }
+        zeroCount
+    }
+
+    /*
+    you're actually supposed to count the number of times any click causes the dial to point at 0, regardless of whether it happens during a rotation or at the end of one.
+     */
+    part(2, 6) {
+        var position = 50
+        var zeroCount = 0
+
+        it.forEach { (direction, value) ->
+            repeat(value) {
+                position = when (direction) {
+                    'L' -> (position - 1).mod(100)
+                    'R' -> (position + 1).mod(100)
+                    else -> error("Invalid direction")
+                }
+
+                if (position == 0) {
+                    zeroCount++
+                }
             }
         }
         zeroCount
